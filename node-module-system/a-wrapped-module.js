3af1533d327d8578141d3ba68c4d863318461ddc
udaibirv <udaibirv@gmail.com>
2021-12-13 15:06:34 -0800

    reviewed node's module system.

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,17 @@
+console.log(typeof module.exports);
+console.log('Value of: ',  module.exports);
+
+console.log(typeof require);
+console.log('Value of: ',  require);
+
+
+console.log(typeof module);
+console.log('Value of: ',  module);
+
+
+console.log(typeof __filename);
+console.log('Value of: ',  __filename);
+
+
+console.log(typeof __dirname);
+console.log('Value of: ',  __dirname);
